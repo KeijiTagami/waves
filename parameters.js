a1c5af9615afb9a14406b3129c8c1601b48ea037
KeijiTagami <--global>
2022-12-07 20:29:41 +0900

    パラメータを増やした

--- parameters.js
+++ parameters.js
@@ -1,3 +1,5 @@
+//学習済みモデルの名前
+MODEL_NAME="n2_l2_s4_lr0.0001_m20_(624_780)"
 //コンテンツの大きさに関する設定
 var WAVE_WIDTH = 624,//波のコンテンツの大きさ(px)(壁ぴったりで(624,780))
     WAVE_HEIGHT =780;
@@ -8,8 +10,8 @@ var WALLHEIGHT_WIDTH=WAVE_WIDTH,//プロジェクターにおける波の高さ�
     WALLHEIGHT_HEIGHT=WAVE_HEIGHT,
     SCALE_WALLHEIGHT=7/12;//プロジェクターから壁へのスケーリング
 
-var OUTPUT_WIDTH = 300,
-    OUTPUT_HEIGHT = 300
+var OUTPUT_WIDTH = 624,
+    OUTPUT_HEIGHT = 780
 
 var WHITE_MARGIN = 20 //モデルによって上下左右クリップされる
 
@@ -22,10 +24,13 @@ var DELTA_TIME = 0.05,  // シミュレーション内でのフレーム時間
     DELTA_WHITE = 3,   // 青何枚につき白を表示するか
     MIN_STOCK = 100,   // これだけためてからスタートする
     MAX_STOCK = 1000,  // 何枚までためるか
-    BATCH = 3          // worker が1回で計算するフレーム数
+    BATCH = 1          // worker が1回で計算するフレーム数
 
 var INIT_FREQ_MIN=-160,//音の大きさ
     INIT_FREQ_MAX=-100;
+
+var AS_HEIGHT=200,
+    AS_WIDTH=600;
 
 var RESOLUTION = 256;
 
